abca2ffa5bdeb82eb30d83f806eaaa72e5258494
Tjaos <th83287716@gmail.com>
2025-10-03 17:11:21 -0300

    feat: turn content in the page center

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,6 +6,10 @@ export const metadata: Metadata = {
 };
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold text-blue-600">Mapa Sensorial TEA</h1>
+    <div className="flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600 flex *:items-center justify-center">
+        Mapa Sensorial TEA
+      </h1>
+    </div>
   );
 }
